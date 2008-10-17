c59edbffd880c65dbaebc382d1b729ec7fe8e7d7
mkashkin <mkashkin@33daf4ba-8911-11dd-b6e4-1fb4a74862d0>
2008-10-17 00:12:57 +0000

    add new api placeholders

---
+++ lib/wowpy/wowhead.py
@@ -0,0 +1,15 @@
+# access to wowhead.com data
+# more info http://www.wowhead.com/?powered
+
+#XML Feeds
+#
+#Also available are our item XML feeds. Every item in the database has a corresponding XML feed. To reach those feeds, just add &xml to the end of any item url. For example:
+#
+#http://www.wowhead.com/?item=192 becomes http://www.wowhead.com/?item=192&xml
+#
+#Alternatively, you can use the actual name of the item (as long as it's spelled correctly). Like so:
+#
+#http://www.wowhead.com/?item=martin+thunder&xml
+#
+#These feeds contain a lot of information about items and can be used by anyone interested in developing 3rd-party applications.
+
