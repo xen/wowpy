7cd76990713109b486ce6e633efd9c63c27711b0
mkashkin <mkashkin@33daf4ba-8911-11dd-b6e4-1fb4a74862d0>
2008-10-28 19:26:24 +0000

    homework ;)

--- demo.py
+++ demo.py
@@ -1,3 +1,3 @@
 from lib.wowpy import Char
 
-pythonista = Char(realm='Kul Tiras', region = 'eu', name='Pythonista')
+pythonista = Char(realm='Kul Tiras', region = 'eu', name='Pythonista', source_file='test/xml/pythonista.xml')
--- lib/wowpy/char.py
+++ lib/wowpy/char.py
@@ -1,16 +1,44 @@
 # -*- coding: utf-8 -*-
+
+import xmltramp 
+import exceptions
+import urllib2
+
 armory_url = 'http://%(region)s.wowarmory.com/%(action)s.xml?%(query)s'
 user_agent = 'Firefox/3.1'
 regions = ('eu', 'us', 'ru', )
 
 class Char:
+    """ Retrive character data from Armory server
     
-    def __init__(self, realm, name, region='eu'):
+       c = Char(realm='Kul Tiras', region = 'eu', name='Pythonista')
+    """
+    
+    def __init__(self, realm, name, region='eu', source_file=None):
         self.realm = realm
         self.name = name
         self.region = region
+        if source_file:
+            self.loadFileData(source_file)
+        else:
+            self.loadData()
         
     def loadData(self):
-        pass
+        """ Get data from server """
+        url = armory_url % {'region': region, 'action': action, 'query': '&'.join('%s=%s' % (key, value) for key, value in query_string.items())}
+        
+        request = urllib2.Request(url, headers={'User-Agent': user_agent})
+        opener = urllib2.build_opener()
+        self.rawdata = opener.open(request).read()
+        
+        self.data = xmltramp.seed(opener.open(request))
+        self.rawdata = ""
     
+    def loadFileData(self, source_file):
+        """ Load data from local file """
+        f = open(source_file, 'r')
+        self.data = xmltramp.seed(opener.open(request))
+        
+        
+        
     
